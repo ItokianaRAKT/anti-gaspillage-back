c67644ac9029b6452344973e75a4d50b8d61cb2d
Sheddy <hei.sheddy.2@gmail.com>
2026-03-22 17:21:24 +0300

    feat(reservations): add collect/not-collected/my-reservations endpoints with auth and ownership checks

--- antiGaspBack/reservations/urls.py
+++ antiGaspBack/reservations/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import ReservationCreateView, ReservationNonRecoveredView
+from .views import (
+    ReservationCreateView,
+    MyReservationsView,
+    ReservationCollectView,
+    ReservationNotCollectedView,
+)
 
 urlpatterns = [
-    path('', ReservationCreateView.as_view(), name='reservation-create'),
-    path('<uuid:pk>/non-recovered/', ReservationNonRecoveredView.as_view(), name='reservation-non-recovered')
+    path('', ReservationCreateView.as_view(), name='reservation_create'),
+    path('my/', MyReservationsView.as_view(), name='my_reservations'),
+    path('<uuid:pk>/collect/', ReservationCollectView.as_view(), name='reservation_collect'),
+    path('<uuid:pk>/not-collected/', ReservationNotCollectedView.as_view(), name='reservation_not_collected'),
 ]
